03c1a36df45629e1ccf71faab8688737d5a8a956
PasOleksandra <153685843+PasOleksandra@users.noreply.github.com>
2026-03-12 16:40:49 +0200

    Add files via upload

---
+++ Lab1/Lab1.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Lab1._1;
+using System;
+using System.Threading;
+
+namespace Lab1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+
+            // Створюємо потоки
+            NumberThread nt = new NumberThread("Потiк чисел");
+            LetterThread lt = new LetterThread("Потiк букв");
+
+            // Запускаємо потоки (використовуємо Start())
+            nt.Start();
+            lt.Start();
+
+            // Чекаємо завершення потоків
+            nt.Thrd.Join();
+            lt.Thrd.Join();
+
+            Console.WriteLine("Завдання закiнчено");
+            Console.ReadLine();
+        }
+    }
+}
