7e31e4b59adb218c2944188351547f88fbbdf1f7
PasOleksandra <153685843+PasOleksandra@users.noreply.github.com>
2026-03-12 16:43:55 +0200

    Add files via upload

---
+++ Lab1/lab1.1/NumberThread.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1._1
+{
+   
+    class NumberThread
+    {
+        public Thread Thrd;
+
+        public NumberThread(string name)
+        {
+            Thrd = new Thread(Run);
+            Thrd.Name = name;
+        }
+
+        public void Start()
+        {
+            Thrd.Start();
+        }
+
+        void Run()
+        {
+            for (int i = 1; i <= 40; i++)
+            {
+                Console.WriteLine(Thrd.Name + ": " + i);
+                Thread.Sleep(200);
+            }
+            Console.WriteLine(Thrd.Name + " завершив роботу");
+        }
+    }
+}
