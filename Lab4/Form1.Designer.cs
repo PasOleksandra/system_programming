0295cb4ae140ef947b8b0856c16cd9fb952288d5
PasOleksandra <153685843+PasOleksandra@users.noreply.github.com>
2026-03-27 13:13:22 +0200

    Lab4

---
+++ Lab4/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.labelPercent = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(59, 397);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(47, 212);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(130, 16);
+            this.labelStatus.TabIndex = 1;
+            this.labelStatus.Text = "Готовий до роботи";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(47, 263);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(153, 16);
+            this.labelResult.TabIndex = 2;
+            this.labelResult.Text = "Результат: очікується";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(610, 397);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 3;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // labelPercent
+            // 
+            this.labelPercent.AutoSize = true;
+            this.labelPercent.Location = new System.Drawing.Point(347, 193);
+            this.labelPercent.Name = "labelPercent";
+            this.labelPercent.Size = new System.Drawing.Size(26, 16);
+            this.labelPercent.TabIndex = 4;
+            this.labelPercent.Text = "0%";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(314, 222);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(100, 23);
+            this.progressBar.TabIndex = 5;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(59, 97);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(103, 20);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(314, 96);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(103, 20);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(565, 97);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(103, 20);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.labelPercent);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Label labelPercent;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ Lab4/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+        // ЗАВДАННЯ 4: Джерело токена для скасування операції
+        private CancellationTokenSource cts;
+
+        // Прапорець для відстеження скасування
+        private bool isCancelled = false;
+
+        // Зберігаємо останній прогрес
+        private int lastProgress = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            buttonCancel.Enabled = false;
+        }
+
+        // ЗАВДАННЯ 1: Кнопка Start
+        private async void buttonStart_Click(object sender, EventArgs e)
+        {
+            isCancelled = false;
+            lastProgress = 0;
+
+            buttonStart.Enabled = false;
+            buttonCancel.Enabled = true;
+
+            labelStatus.Text = "Операція виконується...";
+            labelPercent.Text = "0%";
+            progressBar.Value = 0;
+            labelResult.Text = "Результат: обчислюється...";
+
+            // ЗАВДАННЯ 3: IProgress для відображення прогресу
+            IProgress<int> onChangeProgress = new Progress<int>((i) =>
+            {
+                if (!isCancelled)
+                {
+                    labelPercent.Text = i.ToString() + "%";
+                    progressBar.Value = i;
+                    lastProgress = i;
+                }
+            });
+
+            cts = new CancellationTokenSource();
+
+            try
+            {
+                int result = await Process(100, onChangeProgress, cts.Token);
+
+                // ЗАВДАННЯ 2: Результат (якщо не скасовано)
+                if (!isCancelled)
+                {
+                    labelResult.Text = $"Результат: {result}";
+                    labelStatus.Text = "Операція завершена!";
+                }
+            }
+            
+            finally
+            {
+                buttonStart.Enabled = true;
+                buttonCancel.Enabled = false;
+                cts?.Dispose();
+                cts = null;
+            }
+        }
+
+        // ЗАВДАННЯ 4: Кнопка Cancel
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            if (cts != null && !isCancelled)
+            {
+                // Встановлюємо прапорець
+                isCancelled = true;
+
+                labelResult.Text = $"Результат: ПЕРЕРВАНО (зупинено на {lastProgress}%)";
+                labelStatus.Text = "Операцію перервано!";
+
+                // Скасовуємо операцію
+                cts.Cancel();
+            }
+        }
+
+        // Метод виконання операції
+        private Task<int> Process(int count, IProgress<int> ChangeProgressBar, CancellationToken cancellToken)
+        {
+            return Task.Run(() =>
+            {
+                int i;
+                for (i = 1; i <= count; i++)
+                {
+                    if (cancellToken.IsCancellationRequested)
+                    {
+                        break;
+                    }
+                    ChangeProgressBar.Report(i);
+                    Thread.Sleep(50);
+                }
+                return i - 1;
+            });
+        }
+    }
+}
