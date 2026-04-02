dbf45ea3c7177245af96ac99a0893b701e774a76
PasOleksandra <153685843+PasOleksandra@users.noreply.github.com>
2026-04-02 13:07:10 +0300

    Lab5

---
+++ Lab5/Building.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab5
+{
+    public class Building
+    {
+        public string Name { get; set; }
+        public int YearBuilt { get; set; }
+        public double Height { get; set; }
+        public List<string> Purposes { get; set; }
+
+        public Building()
+        {
+            Name = "Невідома споруда";
+            YearBuilt = 2000;
+            Height = 0;
+            Purposes = new List<string>();
+        }
+
+        public Building(string name, int yearBuilt, double height, List<string> purposes)
+        {
+            Name = name;
+            YearBuilt = yearBuilt;
+            Height = height;
+            Purposes = purposes ?? new List<string>();
+        }
+
+        public void AddPurpose(string purpose)
+        {
+            if (!string.IsNullOrEmpty(purpose))
+                Purposes.Add(purpose);
+        }
+
+        public bool RemovePurpose(string purpose)
+        {
+            return Purposes.Remove(purpose);
+        }
+
+        public int GetAge()
+        {
+            return DateTime.Now.Year - YearBuilt;
+        }
+    }
+}
---
+++ Lab5/Form1.cs
@@ -0,0 +1,62 @@
+﻿using lab5;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace lab5
+    
+{
+    public partial class Form1 : Form
+    {
+        private Building building;
+
+        public Form1()
+        {
+            InitializeComponent();
+ 
+            building = new Building("Ейфелева вежа", 1889, 330, new List<string> { "Туристична", "Оглядова" });
+
+        }
+
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            treeViewProperties.Nodes.Clear();
+
+            DisplayProperties(building);
+        }
+
+        // Завдання 4
+        private void DisplayProperties(object obj)
+        {
+            Type type = obj.GetType();
+
+            // Кореневий вузол
+            TreeNode root = new TreeNode($"Клас: {type.Name}");
+            treeViewProperties.Nodes.Add(root);
+
+            foreach (PropertyInfo prop in type.GetProperties())
+            {
+                object value = prop.GetValue(obj);
+
+                if (value is System.Collections.IEnumerable enumerable && !(value is string))
+                {
+                    TreeNode collectionNode = new TreeNode($"{prop.Name} ({prop.PropertyType.Name})");
+
+                    foreach (var item in enumerable)
+                    {
+                        collectionNode.Nodes.Add($"({item.GetType().Name}) {item}");
+                    }
+
+                    root.Nodes.Add(collectionNode);
+                }
+                else
+                {
+                    root.Nodes.Add($"{prop.Name} ({prop.PropertyType.Name}) = {value}");
+                }
+            }
+
+            treeViewProperties.ExpandAll();
+        }
+    }
+}
